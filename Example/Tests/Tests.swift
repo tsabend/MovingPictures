b6f7e96151b3893bc18f91e00643876c711d47b9
tsabend <tsabend@gmail.com>
2016-08-24 08:19:52 -0400

    Add tests

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -3,8 +3,8 @@
 import Quick
 import Nimble
 import Result
-import MovingPictures
 import AVFoundation
+@testable import MovingPictures
 
 class MovingPicturesSpec: QuickSpec {
     override func spec() {
@@ -28,18 +28,55 @@ class MovingPicturesSpec: QuickSpec {
             }
             
             it("calls the completion with a video that has a duration of the combined images") {
-                expect(1) == 2
                 let writer = MovingPictures(settings: settings)
                 writer.render(imageTimes) { (result: Result<NSURL, NSError>) in
-                    do {
-                        let url = try result.dematerialize()
-                        let asset = AVAsset(URL: url)
-                        let timescale: Double = 600
-                        let time = CMTimeMake(Int64(timescale * 3.7), Int32(timescale))
-                        expect(asset.duration) == time
-                    } catch let error {
-                        print(error)
-                        expect(1) == 2
+                    let url = try! result.dematerialize()
+                    let asset = AVAsset(URL: url)
+                    let time = (3.7).inSeconds
+                    expect(asset.duration) == time
+                }
+            }
+            
+            it("calls the completion with a video of the type specified by the render settings") {
+                let movSettings = RenderSettings(size: images.first!.size, videoFilename: "foo", videoExtension: .MOV)
+                let movWriter = MovingPictures(settings: settings)
+                movWriter.render(imageTimes) { (result: Result<NSURL, NSError>) in
+                    let url = try! result.dematerialize()
+                    expect(url.pathExtension) == movSettings.videoExtension.rawValue
+                }
+                let mp4Settings = RenderSettings(size: images.first!.size, videoFilename: "foo", videoExtension: .MP4)
+                let mp4Writer = MovingPictures(settings: settings)
+                mp4Writer.render(imageTimes) { (result: Result<NSURL, NSError>) in
+                    let url = try! result.dematerialize()
+                    expect(url.pathExtension) == mp4Settings.videoExtension.rawValue
+                }
+            }
+            
+            it("can be called twice with the same filePath without erroring") {
+                let writer1 = MovingPictures(settings: settings)
+                
+                writer1.render(imageTimes) { (result1: Result<NSURL, NSError>) in
+                    
+                    let writer2 = MovingPictures(settings: settings)
+                    writer2.render([(images.first!, 5)]) { (result2: Result<NSURL, NSError>) in
+                        let url1 = try! result1.dematerialize()
+                        let url2 = try! result1.dematerialize()
+                        
+                        expect(NSData(contentsOfURL: url1)) != NSData(contentsOfURL: url2)
+                    }
+                }
+                
+            }
+            
+            context("no images are passed in") {
+                it("calls the completion throwing a VideoWritingError.NoImages error") {
+                    let writer = MovingPictures(settings: settings)
+                    writer.render(imageTimes) { (result: Result<NSURL, NSError>) in
+                        do {
+                            try result.dematerialize()
+                        } catch let error {
+                            expect(error as NSError) == VideoWritingError.NoImages as NSError
+                        }
                     }
                 }
             }
